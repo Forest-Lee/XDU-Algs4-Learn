629c28136c6e5009d8c779f90716c6d6fe722a9c
Forest-Lee <lipeijiegg@gmail.com>
2023-02-09 17:07:12 +0800

    update ch5

---
+++ src/com/franklee/algs4/ch5/LSD.java
@@ -0,0 +1,54 @@
+package com.franklee.algs4.ch5;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class LSD {
+    private LSD() {}
+
+    public static void sort(String[] a, int W) {
+        int N = a.length;
+        int R = 256;
+        String[] aux = new String[N];
+
+        // sort by key-indexed counting on dth character
+        for (int d = W-1; d >= 0; d--) {
+            // compute frequency counts
+            int[] count = new int[R+1];
+            for (int i = 0; i < N; i++) {
+                count[a[i].charAt(d) + 1]++;
+            }
+
+            // compute cumulates
+            for (int r = 0; r < R; r++) {
+                count[r + 1] += count[r];
+            }
+
+            // move data
+            for (int i = 0; i < N; i++) {
+                aux[count[a[i].charAt(d)]++] = a[i];
+            }
+
+            // copy back
+            for (int i = 0; i < N; i++) {
+                a[i] = aux[i];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        int N = a.length;
+
+        int w = a[0].length();
+        for (int i = 0; i < N; i++) {
+            assert a[i].length() == w : "Strings must have fixed length";
+        }
+
+        sort(a, w);
+
+        for (int i = 0; i < N; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+}
---
+++ src/com/franklee/algs4/ch5/MSD.java
@@ -0,0 +1,68 @@
+package com.franklee.algs4.ch5;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class MSD {
+    private MSD() {}
+
+    public static void sort(String[] a) {
+        int N = a.length;
+        String[] aux = new String[N];
+        sort(a, 0, N-1, 0, aux);
+    }
+
+    private static int charAt(String s, int d) {
+        assert d >= 0 && d <= s.length();
+        if (d == s.length()) return -1;
+        return s.charAt(d);
+    }
+
+    private static void sort(String[] a, int lo, int hi, int d, String[] aux) {
+        if (hi <= lo) return;
+        int R = 256;
+        int[] count = new int[R+2];
+
+        // compute frequency counts
+        for (int i = lo; i <= hi; i++) {
+            count[charAt(a[i], d) + 2]++;
+        }
+
+        // compute cumulates
+        for (int r = 0; r < R+1; r++) {
+            count[r+1] += count[r];
+        }
+
+        // for most significant char, 0th position is end-of-string
+        if (d == a[lo].length()) {
+            count[0] = count[R+1];
+            count[R+1] = 0;
+        }
+
+        // move data
+        for (int i = lo; i <= hi; i++) {
+            aux[count[charAt(a[i], d) + 1]++] = a[i];
+        }
+
+        // copy back
+        for (int i = lo; i <= hi; i++) {
+            a[i] = aux[i - lo];
+        }
+
+        // recursively sort for each character (excludes sentinel -1)
+        for (int r = 0; r < R; r++) {
+            sort(a, lo + count[r], lo + count[r+1] - 1, d+1, aux);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        int N = a.length;
+
+        sort(a);
+
+        for (int i = 0; i < N; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+}
